6d6e1c99581549e855c8b2beeb88db78c59f87b8
Kiba <37745559+Kiba1996@users.noreply.github.com>
2019-12-08 17:56:38 +0100

    Scada, read holding registers sa simulatora

---
+++ ModelLabs/ScadaServiceSelfHost/Program.cs
@@ -0,0 +1,31 @@
+﻿using ScadaService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScadaServiceSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using(ScadaService.ScadaService scada = new ScadaService.ScadaService())
+                {
+                    scada.Start();
+                    scada.StartCollectingData();
+                    Console.ReadLine();
+                    
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Scada service failed");
+            }
+        }
+    }
+}
