7cfa9cc7d8b84366ab4cd8776497bb96d2cff3e4
SnezanaDupljanin <SnezanaDupljanin@users.noreply.github.com>
2020-11-26 20:36:54 +0100

    AlarmService and CalculationEngineService

---
+++ ModelLabs/CalculationEngineCloudService/CalculationEngineCloudService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CalculationEngineContracts;
+using CalculationEngineServ;
+using CloudCommon;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using TransactionContract;
+
+namespace CalculationEngineCloudService
+{
+    /// <summary>
+    /// An instance of this class is created for each service replica by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class CalculationEngineCloudService : StatefulService
+    {
+        //private ProcessingToCalculation prToce;
+        //private CeToUI ceToUI;
+        //private CalculationEngine ce;
+
+        public CalculationEngineCloudService(StatefulServiceContext context)
+            : base(context)
+        {
+            //prToce = new ProcessingToCalculation();
+            //ceToUI = new CeToUI();
+            //ce = new CalculationEngine(this.Context); //namjestiti konstruktor
+            //ProcessingToCalculation.CalculationEngine = ce;
+            //CeToUI.Ce = ce;
+        }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <remarks>
+        /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
+        /// </remarks>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        {
+            return new ServiceReplicaListener[0];
+            //return new List<ServiceReplicaListener>
+            //{
+            //    new ServiceReplicaListener(context => this.CreateCalculationEngineListener(context), "CalculationEngineEndpoint"),
+            //    new ServiceReplicaListener(context => this.CreateCalculationEngineUIListener(context), "CalculationEngineUIEndpoint"),
+            //    new ServiceReplicaListener(context => this.CreateCalculationEngineTransactionListener(context), "CalculationEngineTransactionEndpoint"),
+                
+            //    /*ovo ne treba =>*/ //new ServiceReplicaListener(context => this.CreateChooseOptimizationListener(context), "CeChooseOptimizationEndpoint")
+            //};
+        }
+        //private ICommunicationListener CreateCalculationEngineListener(StatefulServiceContext context)
+        //{
+        //    var listener = new WcfCommunicationListener<ICalculationEngineContract>(
+        //                   listenerBinding: Binding.CreateCustomNetTcp(),
+        //                   endpointResourceName: "CalculationEngineEndpoint",
+        //                   serviceContext: context,
+        //                   wcfServiceObject: prToce
+        //    );
+        //    ServiceEventSource.Current.ServiceMessage(context, "Created listener for CalculationEngineEndpoint");
+        //    return listener;
+        //}
+
+        //private ICommunicationListener CreateCalculationEngineUIListener(StatefulServiceContext context)
+        //{
+        //    var listener = new WcfCommunicationListener<ICalculationEngineUIContract>(
+        //                   listenerBinding: Binding.CreateCustomNetTcp(),
+        //                   endpointResourceName: "CalculationEngineUIEndpoint",
+        //                   serviceContext: context,
+        //                   wcfServiceObject: ceToUI
+        //    );
+        //    ServiceEventSource.Current.ServiceMessage(context, "Created listener for CalculationEngineHistoryDataEndpoint");
+        //    return listener;
+        //}
+
+        //private ICommunicationListener CreateCalculationEngineTransactionListener(StatefulServiceContext context)
+        //{
+        //    var listener = new WcfCommunicationListener<ITransactionContract>(
+        //                   listenerBinding: Binding.CreateCustomNetTcp(),
+        //                   endpointResourceName: "CalculationEngineTransactionEndpoint",
+        //                   serviceContext: context,
+        //                   wcfServiceObject: ce
+        //    );
+        //    ServiceEventSource.Current.ServiceMessage(context, "Created listener for CalculationEngineListenerEndpoint");
+        //    return listener;
+        //}
+        /// <summary>
+        /// This is the main entry point for your service replica.
+        /// This method executes when this replica of your service becomes primary and has write status.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Replace the following sample code with your own logic 
+            //       or remove this RunAsync override if it's not needed in your service.
+
+            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
+
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                using (var tx = this.StateManager.CreateTransaction())
+                {
+                    var result = await myDictionary.TryGetValueAsync(tx, "Counter");
+
+                    ServiceEventSource.Current.ServiceMessage(this.Context, "Current Counter Value: {0}",
+                        result.HasValue ? result.Value.ToString() : "Value does not exist.");
+
+                    await myDictionary.AddOrUpdateAsync(tx, "Counter", 0, (key, value) => ++value);
+
+                    // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
+                    // discarded, and nothing is saved to the secondary replicas.
+                    await tx.CommitAsync();
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+    }
+}
